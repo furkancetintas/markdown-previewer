05299a127285f113feda0ce17dee79b93d838e3e
Furkan Çetintaş <66469657+furkancetintas@users.noreply.github.com>
2024-10-22 16:42:57 +0300

    App.js changed, components added!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Editor from './components/Editor';
+import TextOutput from './components/TextOutput';
+import HelpButton from './components/HelpButton';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto py-6 px-4 max-w-[1200px] h-screen">
+      <HelpButton />
+      <Header />
+
+      <div className="mt-4 grid lg:grid-cols-2 gap-4 h-3/4">
+        <Editor />
+        <TextOutput />
+      </div>
+      <Footer />
     </div>
   );
 }
