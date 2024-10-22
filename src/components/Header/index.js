6be4e662f617cffeb164e4d7600b1a0c41b62f84
Furkan Çetintaş <66469657+furkancetintas@users.noreply.github.com>
2024-10-22 16:44:05 +0300

    Header Added!

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div className="text-4xl font-bold text-center text-slate-200">
+      Markdown Previewer
+    </div>
+  );
+}
+
+export default Header;
